cc25e52250e24852fd85199f32b8601f7768cef3
ulugbek101 <thedevu101@gmail.com>
2024-05-29 13:00:45 +0500

    Disable logging in production mode

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import asyncio
 import logging
 
+from environs import Env
+
 from loader import dp, bot
 from handlers.users.start import router
 from utils.set_bot_commands import set_bot_commands
+
+env = Env()
+env.read_env()
 
 
 async def main():
@@ -14,5 +19,8 @@ async def main():
 
 if __name__ == "__main__":
     dp.include_router(router=router)
-    logging.basicConfig(level=logging.INFO)
+
+    if env.str("MODE") == "DEBUG":
+        logging.basicConfig(level=logging.INFO)
+
     asyncio.run(main())
